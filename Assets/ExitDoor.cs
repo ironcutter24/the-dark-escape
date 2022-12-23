8bbae084e44d3b0d7731b95d280ff299dcd7bf38
Damiano Tagliaferri <ironcutter24@gmail.com>
2022-12-23 12:41:27 +0100

    Exit door and logic

---
+++ Assets/ExitDoor.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitDoor : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("you win!");
+        }
+    }
+}
